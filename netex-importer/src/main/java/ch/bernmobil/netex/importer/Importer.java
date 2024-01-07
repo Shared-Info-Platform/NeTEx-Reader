66ac0d804539089d829faba88ac80db0629ddad6
Fabian Landau <fabian.landau@codeca.ch>
2024-01-07 22:16:12 +0100

    some cleanup in Importer

--- netex-importer/src/main/java/ch/bernmobil/netex/importer/Importer.java
+++ netex-importer/src/main/java/ch/bernmobil/netex/importer/Importer.java
@@ -9,7 +9,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 
 import org.codehaus.stax2.XMLInputFactory2;
@@ -42,20 +41,8 @@ public class Importer {
 	private JourneyAggregator aggregator = new JourneyAggregator();
 
 	public static void main(String[] args) throws XMLStreamException, InterruptedException {
-		final File directory = new File("C:\\projects\\bernmobil\\files\\netex\\prod_netex_tt_1.10_che_ski_2024_oev-schweiz__1_1_202312200612");
-//		final File file1 = new File(directory, "PROD_NETEX_TT_1.10_CHE_SKI_2024_OEV-SCHWEIZ_RESOURCE_1_1_202312200612.xml");
-//		final File file2 = new File(directory, "PROD_NETEX_TT_1.10_CHE_SKI_2024_OEV-SCHWEIZ_SITE_1_1_202312200612.xml");
-//		final File file3 = new File(directory, "PROD_NETEX_TT_1.10_CHE_SKI_2024_OEV-SCHWEIZ_SERVICE_1_1_202312200612.xml");
-//		final File file4 = new File(directory, "PROD_NETEX_TT_1.10_CHE_SKI_2024_OEV-SCHWEIZ_SERVICECALENDAR_1_1_202312200612.xml");
-//		final File file5 = new File(directory, "PROD_NETEX_TT_1.10_CHE_SKI_2024_OEV-SCHWEIZ_COMMON_1_1_202312200612.xml");
-//		final File file6 = new File(directory, "PROD_NETEX_TT_1.10_CHE_SKI_2024_OEV-SCHWEIZ_TIMETABLE_1_193_202312200612.xml");
-//
-//		final ImportState state = new ImportState();
-//		final Importer importer = new Importer();
-//		importer.importCommonEntities(Arrays.asList(file1, file2, file3, file4, file5), state);
-//		importer.importServiceJourneys(Arrays.asList(file6), state);
-
 		try {
+			final File directory = new File("C:\\projects\\bernmobil\\files\\netex\\prod_netex_tt_1.10_che_ski_2024_oev-schweiz__1_1_202312200612");
 			new Importer().importDirectory(directory);
 		} catch (Throwable t) {
 			LOGGER.error("import failed", t);
@@ -209,7 +196,6 @@ public class Importer {
 	private int exported;
 
 	private void processJourney(NetexServiceJourney journey) {
-//		System.out.println("new journey " + journey.id);
 		++count1;
 		count2 += journey.calls.size();
 		count3 += journey.availabilityCondition.validDays.size();
@@ -222,59 +208,11 @@ public class Importer {
 			final List<Journey> results = JourneyTransformer.transform(journey);
 			aggregator.aggregateJourneys(results);
 
-//			if (!results.isEmpty() && "820".equals(results.get(0).operatorCode)) {
-				mongoDbWriter.writeJourneys(results);
-				++exported;
-				if (exported % 10000 == 0) {
-					LOGGER.info("Exported {}", exported);
-				}
-//			}
-		});
-//		for (final Journey result : results) {
-//			if ("VBZ".equals(result.operatorShortName) && "N9".equals(result.lineName) && result.operatingDay.equals(LocalDate.of(2024, 3, 30))) {
-//				System.out.println(result.operatingDay + " - " + result.id);
-//			}
-//		}
-	}
-
-	private void importTest(File file) throws XMLStreamException {
-		System.out.println("import " + file);
-
-		final XMLInputFactory2 factory = (XMLInputFactory2)XMLInputFactory2.newInstance();
-		final XMLStreamReader2 reader = factory.createXMLStreamReader(file);
-
-		while (reader.hasNext()) {
-			final int eventType = reader.next();
-			switch (eventType) {
-				case XMLStreamConstants.START_DOCUMENT:
-					System.out.println("start document");
-					break;
-				case XMLStreamConstants.END_DOCUMENT:
-					System.out.println("end document");
-					break;
-				case XMLStreamConstants.START_ELEMENT:
-					System.out.println("start element " + reader.getDepth() + " " + reader.getName() + " / " + reader.getLocalName());
-					for (int i = 0; i < reader.getAttributeCount(); ++i) {
-						System.out.println("  attribute: " + reader.getAttributeName(i) + " / " + reader.getAttributeLocalName(i) + " / " + reader.getAttributeValue(i));
-					}
-					break;
-				case XMLStreamConstants.END_ELEMENT:
-					System.out.println("end element");
-					break;
-				case XMLStreamConstants.ATTRIBUTE:
-					System.out.println("attribute");
-					break;
-				case XMLStreamConstants.CHARACTERS:
-					final String text = reader.getText();
-					System.out.println("characters " + (text.isBlank() ? "" : text));
-					break;
-				case XMLStreamConstants.COMMENT:
-					System.out.println("comment");
-					break;
-				default:
-					System.out.println("something else: " + eventType);
-					break;
+			mongoDbWriter.writeJourneys(results);
+			++exported;
+			if (exported % 10000 == 0) {
+				LOGGER.info("Exported {}", exported);
 			}
-		}
+		});
 	}
 }
