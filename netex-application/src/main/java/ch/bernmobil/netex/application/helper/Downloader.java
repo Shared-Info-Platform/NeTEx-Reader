494cb986377227fb5b8f2afe22694aba73f616a8
Fabian Landau <fabian.landau@codeca.ch>
2025-12-17 22:16:11 +0100

    Downloader: add support for ETag (not yet used)
    also add some more improvements

--- netex-application/src/main/java/ch/bernmobil/netex/application/helper/Downloader.java
+++ netex-application/src/main/java/ch/bernmobil/netex/application/helper/Downloader.java
@@ -3,10 +3,13 @@ package ch.bernmobil.netex.application.helper;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,14 +28,49 @@ public class Downloader {
 	/**
 	 * Downloads a file from an URL, stores it in a temporary directory and returns the file.
 	 */
-	public File downloadFileFromUrlToTemporaryDirectory(URL url) throws IOException {
-		final String fileName = "netex-" + System.currentTimeMillis() + ".tmp";
+	public NetexFile downloadFileFromUrlToTemporaryDirectory(URI uri) throws IOException, InterruptedException {
+		return downloadFileFromUrlToTemporaryDirectoryIfNecessary(uri, null);
+	}
+
+	/**
+	 * Downloads a file from an URI to a temporary directory if the Etag is different. Otherwise it does nothing and returns null.
+	 */
+	public NetexFile downloadFileFromUrlToTemporaryDirectoryIfNecessary(URI uri, String previousEtag)
+			throws IOException, InterruptedException {
+		// Prepare request
+		final HttpRequest.Builder requestBuilder = HttpRequest.newBuilder(uri).GET();
+		if (previousEtag != null) {
+			// Add If-None-Match if we have an ETag
+			requestBuilder.header("If-None-Match", previousEtag);
+		}
+		final HttpRequest request = requestBuilder.build();
+
+		// Send request
+		final HttpClient client = HttpClient.newBuilder().followRedirects(Redirect.NORMAL).build();
+		final HttpResponse<InputStream> response = client.send(request, HttpResponse.BodyHandlers.ofInputStream());
+
+		// Check response code
+		if (response.statusCode() == 304) {
+			return null;
+		} else if (response.statusCode() != 200) {
+			throw new IOException("Unexpected HTTP status " + response.statusCode());
+		}
+
+		// Read metadata
+		final String filename = Path.of(response.uri().getPath()).getFileName().toString();
+		final String newEtag = response.headers().firstValue("ETag").orElse(null);
+		if (newEtag == null) {
+			logger.warn("no ETag defined in response");
+		}
+
+		// Store content as file
 		final File tempDirectory = getTemporaryDirectory();
-		final File tempFile = new File(tempDirectory, fileName);
-		try (final InputStream inputStream = url.openStream()) {
+		final File tempFile = new File(tempDirectory, filename);
+		try (final InputStream inputStream = response.body()) {
 			Files.copy(inputStream, tempFile.toPath());
 		}
-		return tempFile;
+
+		return new NetexFile(tempFile, newEtag);
 	}
 
 	/**
@@ -66,5 +104,8 @@ public class Downloader {
 		} else {
 			return new File(System.getProperty("java.io.tmpdir"));
 		}
+	}
+
+	public record NetexFile(File file, String etag) {
 	}
 }
